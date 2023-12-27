2df2c7cd0968ca30251eb179863749682dbd0e8b
Taxi-Maxim <151550640+Taxi-Maxim@users.noreply.github.com>
2023-12-27 06:17:51 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.getElementById("summit").addEventListener("click", (e) => {
+    e.preventDefault();
+    alert("Данные успешно отправлены!");
+});
+
+document.getElementById("enter").addEventListener("click", () => {
+    alert("Личный кабинет на данный момент недоступен(");
+});
+
+document.getElementById("money").addEventListener("click", () => {
+    alert("Система донатов прилегла отдохнуть(");
+});
